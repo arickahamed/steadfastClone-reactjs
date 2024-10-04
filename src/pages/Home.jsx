887a14f71120ce1000694c12fd3c87be5bdabfda
arick <arickahamed700@gmail.com>
2024-10-05 04:47:36 +0600

    let's build the project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,14 +19,16 @@ const Home = () => {
           </button>
         </div>
         <div className="mt-5 p-3 flex flex-col md:flex-row md:flex-wrap md:justify-center lg:justify-evenly">
-          {cardData.map(info => {
+          {cardData.map((info) => {
             return (
               <div
                 key={info.title}
                 className="flex flex-col p-3 rounded-md border border-green-400 w-[90%] mx-auto mt-5 md:w-[40%] lg:w-[22%] lg:mt-0 shadow-md"
               >
                 <img src={info.image} alt={info.title} />
-                <h3 className="text-[20px] font-bold pb-2 text-center" >{info.title}</h3>
+                <h3 className="text-[20px] font-bold pb-2 text-center">
+                  {info.title}
+                </h3>
                 <p>{info.description}</p>
               </div>
             );
